65c71b9ee522a01119aedf860941875fe2d40ceb
darshana ariyarathna <darshana.ariyarathna@axiatadigitallabs.com>
2024-11-07 00:05:31 +0530

    image converter bug fix

--- image-converter/app.sh
+++ image-converter/app.sh
@@ -26,11 +26,19 @@ file_convertion() {
             mogrify -format $TO_FORMAT "$file"
             
             END_TIME=$(date +%s)
+
+            FILE_SIZE=$(stat -c %s "$file")
             # Calculate the elapsed time in seconds
             ELAPSED_TIME=$((END_TIME - START_TIME))
             # Optional: Print the elapsed time
-            echo "Elapsed time: $ELAPSED_TIME seconds"
+            echo -e -n "\t$ELAPSED_TIME s"
+            # file size
+            FILE_SIZE_KB=$(echo "scale=2; $FILE_SIZE / 1024" | bc)
+            echo -e -n "\t$FILE_SIZE_KB kb"
 
+            # Calculate the processing speed in KB per second
+            PROCESSING_SPEED=$(echo "scale=2; $FILE_SIZE_KB / $ELAPSED_TIME" | bc)
+            echo -e "\t$PROCESSING_SPEED kbps"
         else
             echo "No files with .$FROM_FORMAT extension found"
         fi
